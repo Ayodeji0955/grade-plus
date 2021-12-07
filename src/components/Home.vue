05ce73ec0b1c1b44d2d1bf5a998662b226f9bcb4
Olugbade Ayodeji <olugbade.ayodeji@gmail.com>
2021-12-07 11:53:43 +0100

    Added Test and Git Repo and Updated Home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,7 +8,11 @@
           <h1> 
             <span>Unearth what you were </span>
             <span>not taught in school.</span>
-            </h1>
+          </h1>
+          <h1> 
+            <span>Unearth what you were </span>
+            <span>not taught in school.</span>
+          </h1>
           <p> 
             <span>Unlock the super-learning potentials in you with our courses specially </span> 
             <span>designed for your academic and career excellence.</span>
